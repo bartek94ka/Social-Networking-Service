d13c382d0d1de3a4ea0c996dc751b6adaad3e495
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2017-03-11 12:18:48 +0100

    new project for tests created

---
+++ LocalSocial.Tests/Views/LoginViewTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace LocalSocial.Tests.Views
+{
+    [TestFixture]
+    internal class LoginViewTests
+    {
+        [Test]
+        public void LoginView_NoThrowException()
+        {
+            //Arrange
+            var webDriver = (IWebDriver)new FirefoxDriver();
+            webDriver.Navigate().GoToUrl(@"http://localsocial.azurewebsites.net/#/login");
+            var webElements = webDriver.FindElements(By.TagName("input"));
+
+
+            //Act
+            webElements[0].SendKeys("user1@ls.pl");
+            webElements[1].SendKeys("user1!");
+            var button = webDriver.FindElement(By.TagName("button"));
+            button.Click();
+            //var action = new Action(() => {  });
+
+            //Arrange
+            //Should.NotThrow(action);
+        }
+    }
+}
---
+++ LocalSocial.Tests/Views/RegisterViewTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace LocalSocial.Tests.Views
+{
+    [TestFixture]
+    internal class RegisterViewTests
+    {
+        [Test]
+        public void RegisterView_NoThrowException()
+        {
+            //Arrange
+            var webDriver = (IWebDriver)new FirefoxDriver();
+            webDriver.Navigate().GoToUrl(@"http://localsocial.azurewebsites.net/#/register");
+            var webElements = webDriver.FindElements(By.TagName("input"));
+        }
+    }
+}
